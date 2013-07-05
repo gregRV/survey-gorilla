f17737357559166ad7b743e7fcbc882a9d28e09a
DBC-Apprentice <dbc@dbc.com>
2013-07-05 10:52:35 -0700

    Added initial database migration.

---
+++ db/migrate/20130705103946_initial_setup.rb
@@ -0,0 +1,28 @@
+class InitialSetup < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+    end
+
+    create_table :surveys do |t|
+      t.string :title
+      t.string :description
+      t.references :creator
+    end
+
+    create_table :questions do |t|
+      t.string :text
+      t.references :survey
+    end
+
+    create_table :choices do |t|
+      t. string :text
+      t.references :question
+    end
+
+    create_table :answers do |t|
+      t.references :user
+      t.references :choice
+    end
+  end
+end
