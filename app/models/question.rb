88a2c3277b3f0afaff296665866e43f43e29aaf7
Tambling <dean.tambling@gmail.com>
2013-07-05 11:41:57 -0700

    Added some functionality to Question class.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ActiveRecord::Base
+  belongs_to :survey
+  has_many :choices
+  has_many :answers, through: :choices
+  has_many :users, through: :answers
+  def answered_by?(user)
+
+  end
   # Remember to create a migration!
 end
