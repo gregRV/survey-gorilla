1f82bf209acd9cc64bd880d57af67904bf354516
Apprentice <apprentice@dbc15.local>
2013-07-05 11:42:30 -0700

    working with initial pull

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,42 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+#--- CREATE NEW SURVEY
+get '/surveys/new' do
+
+end
+
+post '/surveys' do
+
+end
+
+#--- SHOW SURVEY
+get '/surveys/:id' do |id|
+
+end
+
+post '/surveys/:id' do |id|
+
+end  
+
+get '/users/new' do
+
+end
+
+post '/users' do
+
+end 
+
+#--- NEW SESSIONS
+get '/sessions/new' do
+  "Hello World"
+end
+
+post '/sessions' do
+
+end
+
+get '/sessions/logout' do
+  session.clear
 end
